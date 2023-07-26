eed957e792d9a2f63298bb77b6effc7362a444b9
ivan <valenciaivan21@gmail.com>
2023-07-26 17:28:43 +0200

    carrusel deportivo

--- src/components/base/landing/testimonials.js
+++ src/components/base/landing/testimonials.js
@@ -1,154 +1,147 @@
-import { ReactNode } from "react";
+'use client'
+
+import React from 'react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import {
   Box,
-  Flex,
+  IconButton,
+  useBreakpointValue,
+  Stack,
   Heading,
   Text,
-  Stack,
   Container,
-  Avatar,
-  useColorModeValue,
-} from "@chakra-ui/react";
+  Link,
+} from '@chakra-ui/react'
+// Here we have used react-icons package for the icons
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi'
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick'
 
-const Testimonial = ({ children }) => {
-  return <Box maxW={{ base: "100%", md: "33%" }}>{children}</Box>;
-};
-
-const TestimonialContent = ({ children }) => {
-  return (
-    <Stack
-      bg={useColorModeValue("white", "gray.800")}
-      boxShadow={"lg"}
-      p={10}
-      rounded={"xl"}
-      align={"center"}
-      pos={"relative"}
-      minH={230}
-      _after={{
-        content: `""`,
-        w: 0,
-        h: 0,
-        borderLeft: "solid transparent",
-        borderLeftWidth: 16,
-        borderRight: "solid transparent",
-        borderRightWidth: 16,
-        borderTop: "solid",
-        borderTopWidth: 16,
-        borderTopColor: useColorModeValue("white", "gray.800"),
-        pos: "absolute",
-        bottom: "-16px",
-        left: "50%",
-        transform: "translateX(-50%)",
-      }}
-    >
-      {children}
-    </Stack>
-  );
-};
-
-const TestimonialHeading = ({ children }) => {
-  return (
-    <Heading as={"h3"} fontSize={"xl"}>
-      {children}
-    </Heading>
-  );
-};
-
-const TestimonialText = ({ children }) => {
-  return (
-    <Text
-      textAlign={"center"}
-      color={useColorModeValue("gray.600", "gray.400")}
-      fontSize={"sm"}
-    >
-      {children}
-    </Text>
-  );
-};
-
-const TestimonialAvatar = ({ src, name, title }) => {
-  return (
-    <Flex align={"center"} mt={8} direction={"column"}>
-      <Avatar src={src} alt={name} mb={2} />
-      <Stack spacing={-1} align={"center"}>
-        <Text fontWeight={600}>{name}</Text>
-        <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
-          {title}
-        </Text>
-      </Stack>
-    </Flex>
-  );
-};
-
-export default function Testimonials() {
-  return (
-    <Box bg={useColorModeValue("gray.100", "gray.700")}>
-      <Container maxW={"7xl"} py={20} as={Stack} spacing={12}>
-        <Stack spacing={0} align={"center"}>
-          <Heading mb={3}>Nuestros clientes dicen...</Heading>
-          <Text>
-            Profesionales, empresas e instituciones educativas que han utilizado
-            nuestros servicios
-          </Text>
-        </Stack>
-        <Stack
-          direction={{ base: "column", md: "row" }}
-          spacing={{ base: 10, md: 4, lg: 10 }}
-        >
-          <Testimonial>
-            <TestimonialContent>
-              <TestimonialHeading>Gané en todo</TestimonialHeading>
-              <TestimonialText>
-                Conseguí encontrar las oportunidades que más se ajustaban a lo
-                que buscaba. Me prepararon curriculum, prueba técnica y
-                entrevistas con la empresa, y en nada estaba trabajando con
-                ellos.
-              </TestimonialText>
-            </TestimonialContent>
-            <TestimonialAvatar
-              src={
-                "https://media.licdn.com/dms/image/C4D03AQElft4xFQXoFA/profile-displayphoto-shrink_100_100/0/1662477945078?e=1686787200&v=beta&t=LthyXN135XW8Xb4juKE54qL2HM03Jt5bOkDbemi9Gd8"
-              }
-              name={"Miguel Muñoz"}
-              title={"Back end developer"}
-            />
-          </Testimonial>
-          <Testimonial>
-            <TestimonialContent>
-              <TestimonialHeading>Fácil lo dificil</TestimonialHeading>
-              <TestimonialText>
-                Llevaba 3 meses buscando un desarrollador con otras consultoras,
-                hasta que encontre a boomiuz. En dos semanas tenía el perfil que
-                quería y en menos de un mes ya estaba en plantilla.
-              </TestimonialText>
-            </TestimonialContent>
-            <TestimonialAvatar
-              src={
-               "https://estaticos-cdn.prensaiberica.es/clip/1683fce3-29b9-4d56-905d-98eb2d00642e_21-9-aspect-ratio_default_0.jpg"
-              }
-              name={"Juan Carlos Bernabé"}
-              title={"CEO de Bernatrelis S.L"}
-            />
-          </Testimonial>
-          <Testimonial>
-            <TestimonialContent>
-              <TestimonialHeading>Inmejorable</TestimonialHeading>
-              <TestimonialText>
-                Boomiuz compartió con todos nuestros alumnos un pequeño taller
-                sobre como preparar el curriculum y configurar su linkedIn. La
-                verdad es que todos acabamos súper contentos.
-              </TestimonialText>
-            </TestimonialContent>
-            <TestimonialAvatar
-              src={
-                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-              }
-              name={"Cristina Pellicer"}
-              title={"Profesora de bootcamp"}
-            />
-          </Testimonial>
-        </Stack>
-      </Container>
-    </Box>
-  );
+// Settings for the slider
+const settings = {
+  dots: true,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
 }
+
+export default function CaptionCarousel() {
+  // As we have used custom buttons, we need a reference variable to
+  // change the state
+  const [slider, setSlider] = React.useState(null)
+
+  // These are the breakpoints which changes the position of the
+  // buttons as the screen size changes
+  const top = useBreakpointValue({ base: '90%', md: '50%' })
+  const side = useBreakpointValue({ base: '30%', md: '40px' })
+
+  // This list contains all the data for carousels
+  // This can be static or loaded from a server
+  const cards = [
+    {
+      title: 'Design Projects 1',
+      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+      image:
+        'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+      web: 'https:/www.google.es',
+    },
+    {
+      title: 'Design Projects 2',
+      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+      image:
+        'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
+      web:'https:www.youtube.es'
+    },
+    {
+      title: 'Design Projects 3',
+      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+      image:
+        'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+      web: 'https:/www.twitter.es',
+    },
+  ]
+
+  const goToWeb = (web) => {
+    console.log(web)
+    
+  }
+  return (
+    <Box position={'relative'} height={'500px'} width={'full'} overflow={'hidden'}>
+      {/* CSS files for react-slick */}
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+
+      />
+      {/* Left Icon */}
+      <IconButton
+        aria-label="left-arrow"
+        variant="ghost"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickPrev()}>
+        <BiLeftArrowAlt size="40px" />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        aria-label="right-arrow"
+        variant="ghost"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickNext()}>
+        <BiRightArrowAlt size="40px" />
+      </IconButton>
+      {/* Slider */}
+      <Slider {...settings} ref={(slider) => setSlider(slider)}>
+      {cards.map((card, index) => (
+        <Box
+          key={index}
+          height={'6xl'}
+          position="relative"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+          backgroundImage={`url(${card.image})`}
+        >
+          {/* This is the block you need to change, to customize the caption */}
+          <Container size="container.lg" height="600px" position="relative">
+            <Stack
+              spacing={6}
+              w={'full'}
+              maxW={'lg'}
+              position="absolute"
+              top="50%"
+              transform="translate(0, -50%)"
+            >
+              <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                {card.title}
+              </Heading>
+              <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
+                {card.text}
+              </Text>
+             <Link  href='www.googgle.es'>
+              Visitar web <ExternalLinkIcon mx='2px' />
+              </Link>
+            </Stack>
+          </Container>
+        </Box>
+      ))}
+    </Slider>
+  </Box>
+);}
